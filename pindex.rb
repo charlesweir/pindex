df810c8c6624e73ef8dbfb17beb6bf909fa5b423
Charles Weir <797903+charlesweir@users.noreply.github.com>
2020-05-25 09:20:25 +0100

    Update pindex.rb

--- pindex.rb
+++ pindex.rb
@@ -1,14 +1,24 @@
-%w[benchmark pp rexml/document net/http net/https].each { |x| require x }
+#!/usr/bin/ruby
+# Script to create URL files with the appropriate timestamp for each of one's Pinboard bookmarks
+# The <root directory> must have directories pinboard and pinboard/bookmarks
+#
+# Original from https://github.com/kamiller/pindex/
+# Modified to run as executable script.
 
-user = ARGV[0]
-pass = ARGV[1]
-baseDir = ARGV[2]
+%w[benchmark pp rexml/document net/http net/https time fileutils].each { |x| require x }
 
+# Original Usage: <script> <username> <password> <root directory>
+#user = ARGV[0]
+#pass = ARGV[1]
+#bookmarksDir = ARGV[2]
 
-baseDir += "/pinboard/"
-xmlFile = baseDir+"bookmarks.xml"
-bookmarksDir = baseDir+"/bookmarks/"
+user = "Insert pinboard username here"
+pass = "Insert pinboard password Here"
+bookmarksDir = "/Users/changeMe/Documents/pinboard"
 
+xmlFile = "/tmp/bookmarks.xml"
+
+FileUtils.rm Dir.glob(bookmarksDir + '/*.url')
 
 http = Net::HTTP.new("api.pinboard.in",443)
 req = Net::HTTP::Get.new("/v1/posts/all")
@@ -16,23 +26,26 @@ http.use_ssl = true
 req.basic_auth "#{user}", "#{pass}"
 response = http.request(req)
 
-puts("downloading bookmarks")
+# puts("downloading bookmarks")
 f = File.new(xmlFile,"w+")
 f.puts response.body
 f.close
 
 xml = File.read(xmlFile)
-  puts("parsing bookmarks")
+  # puts("parsing bookmarks")
   doc, posts = REXML::Document.new(xml), []
   doc.elements.each('posts/post') do |p|
     posts << p.attributes
    
-   x = "pb-"+p.attributes["description"].gsub(/[\x00-\x20\x7e-\xff\s\W]/,"_")
+   x = "pb-"+p.attributes["description"].gsub(/[^a-z0-9-]/imu,"_")
    x = x[0,100]
    
-   fn = bookmarksDir + x +".url"
+   fn = bookmarksDir + "/" + x +".url"
     u = File.new(fn, "w")
     u.puts "[InternetShortcut]\nURL=%s\nHASH=%s\nTAGS=%s\nDATETIME=%s\nDESCRIPTION=%s\nEXTENDED=%s\nMETA=%s\nORIGIN=pinboard" % [p.attributes["href"],p.attributes["hash"],p.attributes["tag"],p.attributes["time"],p.attributes["description"],p.attributes["extended"],p.attributes["meta"]]
     u.close
+    time=Time.iso8601(p.attributes["time"])
+    FileUtils.touch fn, :mtime => time
      
   end
+FileUtils.rm xmlFile
